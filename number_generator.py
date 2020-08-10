f6ee2a4db85f0021fc37571e0ba309942fd9e4bc
machukhinktato <tarabrinmv@gmail.com>
2020-08-10 11:02:43 +0300

    numberic generator done

--- number_generator.py
+++ number_generator.py
@@ -4,15 +4,17 @@ import random
 def random_nums_generator(lesser_value, greater_value):
     if lesser_value > greater_value:
         lesser_value, greater_value = greater_value, lesser_value
-    random_numeric, counter = [], 1
+    random_numeric_list, random_numeric_dict, counter = [], {}, 1
     while counter <= greater_value:
         if lesser_value != 0 and greater_value != 0:
-            random_numeric.append(random.randint(lesser_value, greater_value))
+            our_number = random.randint(lesser_value, greater_value)
+            random_numeric_list.append(our_number)
+            random_numeric_dict.update({"elem_" + str(our_number): our_number})
             counter += 1
         else:
             raise ValueError
-
-    print(random_numeric)
+    print(random_numeric_list)
+    print(random_numeric_dict)
 
 
 if __name__ == '__main__':
