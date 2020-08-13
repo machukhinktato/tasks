738c1371bea1dc77ed9026fea7ed220b95683761
machukhinktato <tarabrinmv@gmail.com>
2020-08-13 23:06:06 +0300

    deposit task with alternative option of work

--- bank_deposit.py
+++ bank_deposit.py
@@ -33,5 +33,40 @@ def bank_deposit(deposit, duration):
             raise ValueError
 
 
+# alternative option
+
+def get_percent(amount, months):
+    """in touch with percentage"""
+    if months not in [6, 12, 24]:
+        return False
+
+    rates = (
+        {'begin_sum': 1000, 'end_sum': 10000, 6: 5, 12: 6, 24: 5},
+        {'begin_sum': 10000, 'end_sum': 100000, 6: 6, 12: 7, 24: 6.5},
+        {'begin_sum': 100000, 'end_sum': 1000000, 6: 7, 12: 8, 24: 7.5},
+    )
+
+    for rate in rates:
+        if rate['begin_sum'] <= amount < rate['end_sum']:
+            return rate[months]
+
+    return False
+
+
+def deposit(amount, months):
+    """summary of profit"""
+    percent = get_percent(amount, months)
+    if not percent:
+        print('tariff is not liquid')
+
+    total = amount
+    for _ in range(months):
+        profit = total * percent / 100 / 12
+        total += profit
+
+    return (round(total, 2))
+
+
 if __name__ == '__main__':
     print(bank_deposit(1000000, 12))
+    print(deposit(10000, 24))
