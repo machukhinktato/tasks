a1fe209e312a58a97f2eadbf98a0fadfaaf91903
machukhinktato <tarabrinmv@gmail.com>
2020-08-14 14:32:00 +0300

    with private encapsulation program raise error

---
+++ oop/inheritance_plus_encapsulation.py
@@ -0,0 +1,27 @@
+class ItemDiscount:
+    """ create class and his subclass, which will display
+     parent attributes information, attributes will be encapsulated """
+
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+    def __str__(self):
+        return f'{self.__name} {self.__price}'
+
+
+class ItemDiscountReport(ItemDiscount):
+
+    def get_parent_data(self):
+        return f'{self.__name} with price {self.__price} rubles'
+
+
+if __name__ == '__main__':
+    slot_1 = ItemDiscountReport('"Garmin 985"', 36306)
+    try:
+        print(slot_1.get_parent_data())
+    except:
+        print('сгенерирована ошибка выполнения')
+    # slot_2 = ItemDiscount('"Garmin 985"', 36306)
+    # print(slot_2._ItemDiscount__name, slot_2._ItemDiscount__price)
+    # print(slot_2.__name, slot_2.__price)
