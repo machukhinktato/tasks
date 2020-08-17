58f2b5602996c3cd55569c331eafb311e82876e9
machukhinktato <tarabrinmv@gmail.com>
2020-08-17 16:44:22 +0300

    done

--- orm/6_inher_with_poly.py
+++ orm/6_inher_with_poly.py
@@ -26,7 +26,7 @@ class ItemDiscount:
             return f'{self.__name} price is: {self.__price}'
 
 
-class ItemDiscountReport(ItemDiscount):
+class ItemDiscountReport_1(ItemDiscount):
 
     def __init__(self, name, price, discount=0):
         super().__init__(name, price)
@@ -41,9 +41,41 @@ class ItemDiscountReport(ItemDiscount):
     def get_parent_data(self):
         return f'{self.call_name()} with price {self.call_price()} rubles'
 
+    def get_info(self):
+        return f'{self.call_name()}'
+
+
+class ItemDiscountReport_2(ItemDiscount):
+
+    def __init__(self, name, price, discount=0):
+        super().__init__(name, price)
+        self.discount = price / 100 * discount
+
+    def __str__(self):
+        if self.discount != 0:
+            return f'price with your discount - {self.call_price() - self.discount}'
+        else:
+            return f'{self.call_price()}'
+
+    def get_parent_data(self):
+        return f'{self.call_name()} with price {self.call_price()} rubles'
+
+    def get_info(self):
+        return f'{self.call_price()}'
+
+
+def value_handler(value):
+    return value.get_info()
+
 
 if __name__ == '__main__':
-    # slot_1 = ItemDiscountReport('"Garmin 985"', 36306, 10)
-    slot_1 = ItemDiscountReport('"Garmin 985"', 36306)
-    print(slot_1)
-    print(slot_1.call_price(37000))
+    slot_1 = ItemDiscountReport_1('"Garmin 985"', 36306, 10)
+    slot_2 = ItemDiscountReport_2('"Garmin 985"', 36306, 10)
+    # slot_1 = ItemDiscountReport_1('"Garmin 985"', 36306)
+    # slot_2 = ItemDiscountReport_2('"Garmin 985"', 36306)
+    print(f'primary option {slot_1.get_info()}')
+    print(f'primary option {slot_2.get_info()}')
+    for value in slot_1, slot_2:
+        print(f'second option {value.get_info()}')
+    print(f'third option {value_handler(slot_1)}')
+    print(f'third option {value_handler(slot_2)}')
