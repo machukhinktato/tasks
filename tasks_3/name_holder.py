2f9140595c39869fe5dec6a2e33b2bb7f8cac72d
machukhinktato <tarabrinmv@gmail.com>
2020-08-19 21:04:45 +0300

    done

--- tasks_3/name_holder.py
+++ tasks_3/name_holder.py
@@ -4,10 +4,13 @@ import re
 
 def name_holder(path):
     """function which shown file name from path provided"""
-    pattern_filename = r'\\+|/|\.'
-    result = re.split(pattern_filename, path.lower())
+    pattern = r'\\+|/|\.'
+    result = re.split(pattern, path.lower())
     return result[-2]
 
 
 if __name__ == '__main__':
     print(name_holder('../mainapp/views.py'))
+    print(name_holder('../cmd.exe'))
+    print(name_holder('~/setup.exe'))
+    print(name_holder('C:\\IT\\PythonProjects\\tasks\\palindrome.py'))
