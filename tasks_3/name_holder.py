3784c0fa1b2f19e56e91ccf3ef77446693249a54
machukhinktato <tarabrinmv@gmail.com>
2020-08-19 21:14:05 +0300

    done

--- tasks_3/name_holder.py
+++ tasks_3/name_holder.py
@@ -12,5 +12,6 @@ def name_holder(path):
 if __name__ == '__main__':
     print(name_holder('../mainapp/views.py'))
     print(name_holder('../cmd.exe'))
+    print(name_holder('../bootstrap.js'))
     print(name_holder('~/setup.exe'))
     print(name_holder('C:\\IT\\PythonProjects\\tasks\\palindrome.py'))
