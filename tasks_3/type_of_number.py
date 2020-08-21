9f6c9cb1624be98aa036297a42c0f22ba6d6568c
machukhinktato <tarabrinmv@gmail.com>
2020-08-21 16:38:19 +0300

    done

--- tasks_3/type_of_number.py
+++ tasks_3/type_of_number.py
@@ -1,23 +1,22 @@
 import re
 
 
-def type_checker(testing):
-    value = str(testing)
+def type_checker():
+    value = input('Введите число: ')
     if value.isdecimal():
-        return int(value)
+        return f'{int(value)} это целое число.'
     elif value.isalpha():
-        return value
+        return f'{value} это буквенные символы/слово, программа работает только с целыми и дробными числами.'
     else:
-        a,b = value.split('.')
-        if a > b:
-            return 'a > b'
-        elif a < b:
-            return 'a < b'
+        a, b = value.split('.')
+        if a == b:
+            return f'Число {float(value)} дробное.\nЛевая и правая части совпадают'
         else:
-            return 'a = b'
+            return f'Число {float(value)} дробное. \nЛевая и правая части не совпадают'
 
 
 if __name__ == '__main__':
-    print(type_checker('3.14'))
-    print(type_checker('3'))
-    print(type_checker('Lenin'))
+    print(type_checker())
+    # print(type_checker('3.14'))
+    # print(type_checker('3'))
+    # print(type_checker('Lenin'))
