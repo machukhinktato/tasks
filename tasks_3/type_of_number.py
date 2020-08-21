864217a4763ea01b2bbdff887ad4f3a090ff280e
machukhinktato <tarabrinmv@gmail.com>
2020-08-21 16:59:12 +0300

    done

--- tasks_3/type_of_number.py
+++ tasks_3/type_of_number.py
@@ -1,22 +1,21 @@
-import re
-
-
 def type_checker():
-    value = input('Введите число: ')
-    if value.isdecimal():
-        return f'{int(value)} это целое число.'
-    elif value.isalpha():
-        return f'{value} это буквенные символы/слово, программа работает только с целыми и дробными числами.'
-    else:
-        a, b = value.split('.')
-        if a == b:
-            return f'Число {float(value)} дробное.\nЛевая и правая части совпадают'
+    try:
+        value = input('Введите число: ')
+        if value.isdecimal():
+            return f'{int(value)} это целое число.'
+        elif value.isalpha():
+            return f'{value} это буквенные символы/слово.\n' \
+                   f'Программа работает только с целыми и дробными числами.\n' \
+                   f'Пограмма завершена.'
         else:
-            return f'Число {float(value)} дробное. \nЛевая и правая части не совпадают'
+            a, b = value.split('.')
+            if a == b:
+                return f'Число {float(value)} дробное.\nЛевая и правая части совпадают'
+            else:
+                return f'Число {float(value)} дробное. \nЛевая и правая части не совпадают'
+    except:
+        return 'Вы указали что-то непонятное, программа завершена'
 
 
 if __name__ == '__main__':
     print(type_checker())
-    # print(type_checker('3.14'))
-    # print(type_checker('3'))
-    # print(type_checker('Lenin'))
