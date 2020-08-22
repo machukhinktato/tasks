2e8b19a06dcee2bbf0745f27abc714862a6a056f
machukhinktato <tarabrinmv@gmail.com>
2020-08-22 13:32:01 +0300

    in progress

--- tasks_3/zip_function.py
+++ tasks_3/zip_function.py
@@ -1,6 +1,14 @@
-def zip_function():
-    key_list = int(input('Введите число: '))
-    return key_list
+def zip_function(*args):
+    # choise = 'n'
+    # wish_to_stop, key_list = None, []
+    # while choise != wish_to_stop:
+    #     wish_to_stop = input('Требуется объединить данные? y/n ').lower()
+    #     if 'y':
+    #         key_list.append(int(input('Введите число: ')))
+        # return type(key_list)
+    first_data = list(args)
+    return first_data
+
 
 if __name__ == '__main__':
-    print(zip_function())
+    print(zip_function(1,2,3,4,5,6))
