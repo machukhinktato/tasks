e4a3ceecf147db285e511852291ccddbc27ca6a1
machukhinktato <tarabrinmv@gmail.com>
2020-08-22 16:36:49 +0300

    half done, other in progress

--- tasks_3/zip_function.py
+++ tasks_3/zip_function.py
@@ -1,7 +1,12 @@
 def zip_function(*args):
-    keys, values = list(args)
-    return dict(zip(keys, values))
+    keys, values = args
+    keys, values = list(keys), list(values)
+    if len(keys) > len(values):
+        additional_values = len(keys) - len(values)
+        for value in range(additional_values):
+            values.append(None)
+        return dict(zip(keys, values))
 
 
 if __name__ == '__main__':
-    print(zip_function((1,2,3,4,5,6),(1,2,2,3,4,5)))
+    print(zip_function((1, 2, 3, 4, 5, 6, 7, 8), (1, 2, 2, 3, 4, 5)))
