8a7bf58c82a3289b991e3842aba46f81daddd23d
machukhinktato <tarabrinmv@gmail.com>
2020-08-23 17:27:23 +0300

    in progress

--- tasks_3/in_touch_with_file.py
+++ tasks_3/in_touch_with_file.py
@@ -1,3 +1,6 @@
+import random
+
+
 def abrakadabra(name='default.txt'):
     try:
         # with open(name, 'x', encoding='utf8') as textfile:
@@ -9,8 +12,17 @@ def abrakadabra(name='default.txt'):
 
 
 def randomizer():
-    pass
+    numbers = [random.randint(0, 99) for _ in range(10)]
+    letters = []
+    for i in range(10):
+        numbers_of_ascii = [random.randint(65, 122) for _ in range(10)]
+        letras = []
+        for i in numbers_of_ascii:
+            letras.append(chr(i))
+        letters.append(''.join(letras))
+    return list(zip(numbers, letters))
 
 
 if __name__ == '__main__':
     abrakadabra()
+    print(randomizer())
