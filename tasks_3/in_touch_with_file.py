133b7b0b0c43d51fd03467a26b0a4b2473e859d7
machukhinktato <tarabrinmv@gmail.com>
2020-08-23 15:33:01 +0300

    in progress

--- tasks_3/in_touch_with_file.py
+++ tasks_3/in_touch_with_file.py
@@ -1,6 +1,16 @@
-try:
-    with open('text.txt', 'x', encoding='utf8') as textfile:
-        text = 'text'
-        textfile.write(text)
-except:
-    print('there is the same')
+def abrakadabra(name='default.txt'):
+    try:
+        # with open(name, 'x', encoding='utf8') as textfile:
+        with open(name, 'w', encoding='utf8') as textfile:
+            text = 'test'
+            textfile.write(text)
+    except:
+        print('there is the same')
+
+
+def randomizer():
+    pass
+
+
+if __name__ == '__main__':
+    abrakadabra()
