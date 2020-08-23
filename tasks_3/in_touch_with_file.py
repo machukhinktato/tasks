cdc4e3943af59f18596ffb80e7b399f431c9f522
machukhinktato <tarabrinmv@gmail.com>
2020-08-23 17:46:10 +0300

    in progress

--- tasks_3/in_touch_with_file.py
+++ tasks_3/in_touch_with_file.py
@@ -3,10 +3,8 @@ import random
 
 def abrakadabra(name='default.txt'):
     try:
-        # with open(name, 'x', encoding='utf8') as textfile:
         with open(name, 'w', encoding='utf8') as textfile:
-            text = 'test'
-            textfile.write(text)
+            textfile.write('This is my practical work\n')
     except:
         print('there is the same')
 
@@ -16,12 +14,15 @@ def randomizer():
     letters = []
     for i in range(10):
         numbers_of_ascii = [random.randint(65, 122) for _ in range(10)]
-        letras = []
+        ascii_call = []
         for i in numbers_of_ascii:
-            letras.append(chr(i))
-        letters.append(''.join(letras))
-    return list(zip(numbers, letters))
-
+            ascii_call.append(chr(i))
+        letters.append(''.join(ascii_call))
+    banana = list(zip(numbers, letters))
+    with open('default.txt', 'a', encoding='utf8') as textfile:
+        for i in range(len(banana)):
+            textfile.writelines(str(banana[i]) + '\n')
+        # return banana[1]
 
 if __name__ == '__main__':
     abrakadabra()
