525a4a2b51c30b8dd42cc0743fffc5eb0c35a132
machukhinktato <tarabrinmv@gmail.com>
2020-08-23 13:38:15 +0300

    done

--- tasks_3/zip_function.py
+++ tasks_3/zip_function.py
@@ -1,5 +1,12 @@
+import sys
+
+
 def zip_function(*args):
-    keys, values = args
+    if args:
+        keys, values = args
+    else:
+        keys = task_former()
+        values = task_former()
     keys, values = list(keys), list(values)
     if len(keys) > len(values):
         additional_values = len(keys) - len(values)
@@ -10,7 +17,26 @@ def zip_function(*args):
         return dict(zip(keys, values))
 
 
+def task_former():
+    value = 0
+    values = []
+    print('Инструкция(напоминание):\n'
+          'Создайте список ключей, вводя по одному числу за раз\n'
+          'Для перехода к списку значений, нажмите любой буквенный знак\n'
+          'Заполняйте список значений аналогично, вторичное использование\n'
+          'Любого буквенного символа завершит программу')
+    while type(value) != str:
+        try:
+            value = int(input('Введите число: '))
+            values.append(value)
+        except:
+            return values
+            sys.exit()
+
+
 if __name__ == '__main__':
+    print(zip_function())
     print(zip_function((1, 2, 3, 4, 5, 6, 7, 8), (1, 2, 2, 3, 4, 5)))
     print(zip_function((1, 2, 3, 4, 5, 6), (1, 2, 2, 3, 4, 5)))
     print(zip_function((1, 2, 3, 4), (1, 2, 2, 3, 4, 5)))
+    print(zip_function(('Маша', 'Миша', 'Едет', 'Крыша', 'Голова-то', 'Из Гашиша'), ('Нет', 'Свободы', 'В', 'Беларуси')))
