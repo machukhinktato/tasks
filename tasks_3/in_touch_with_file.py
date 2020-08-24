83908d6e2095dd12444d9718526ae86d392648f1
machukhinktato <tarabrinmv@gmail.com>
2020-08-24 13:49:47 +0300

    in progress

--- tasks_3/in_touch_with_file.py
+++ tasks_3/in_touch_with_file.py
@@ -1,12 +1,17 @@
 import random
+import sys
 
 
-def abrakadabra(name='default.txt'):
+def abrakadabra(data, name='default.txt'):
     try:
-        with open(name, 'w', encoding='utf8') as textfile:
-            textfile.write('This is my practical work\n')
+        with open(name, 'x', encoding='utf8') as textfile:
+            print('This is my practical work\n', data, file=textfile, end='\n')
     except:
-        print('there is the same')
+        print('file already exists and will be added')
+        with open(name, 'a', encoding='utf8') as textfile:
+            print(data, file=textfile, end='\n')
+    finally:
+        sys.exit()
 
 
 def randomizer():
@@ -14,16 +19,17 @@ def randomizer():
     letters = []
     for i in range(10):
         numbers_of_ascii = [random.randint(65, 122) for _ in range(10)]
-        ascii_call = []
-        for i in numbers_of_ascii:
-            ascii_call.append(chr(i))
-        letters.append(''.join(ascii_call))
-    banana = list(zip(numbers, letters))
-    with open('default.txt', 'a', encoding='utf8') as textfile:
-        for i in range(len(banana)):
-            textfile.writelines(str(banana[i]) + '\n')
-        # return banana[1]
+        ascii_generator = []
+        [ascii_generator.append(chr(number_of_letter)) for number_of_letter in numbers_of_ascii]
+        letters.append(''.join(ascii_generator))
+    file_to_write = list(zip(numbers, letters))
+    string_to_write = [f'{str(value)}' for value in file_to_write]
+    print(string_to_write)
+    while i < len(string_to_write):
+        abrakadabra(''.join(string_to_write[i]))
+        i += 1
+    return 'objective is done'
+
 
 if __name__ == '__main__':
-    abrakadabra()
-    print(randomizer())
+    randomizer()
