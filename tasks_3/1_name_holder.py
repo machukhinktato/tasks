91411dd9d5ec8a349e0beb105d8fab8726c41062
machukhinktato <tarabrinmv@gmail.com>
2020-08-25 16:06:23 +0300

    done

--- tasks_3/1_name_holder.py
+++ tasks_3/1_name_holder.py
@@ -8,8 +8,14 @@ def name_holder(path):
     return result[-2]
 
 
+def get_file_name(path):
+    # alternative version
+    return path.split('/')[-1].split('.')[0]
+
+
 if __name__ == '__main__':
     print(name_holder('../mainapp/views.py'))
+    print(get_file_name('../mainapp/views.py'))
     print(name_holder('../cmd.exe'))
     print(name_holder('~//setup.exe'))
     print(name_holder('C:\\IT\\PythonProjects\\tasks\\palindrome.py'))
