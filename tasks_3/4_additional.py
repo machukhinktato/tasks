98255e5546037199aa121390dd8d068c99285d5b
machukhinktato <tarabrinmv@gmail.com>
2020-08-25 17:08:53 +0300

    alternative version done

--- tasks_3/4_additional.py
+++ tasks_3/4_additional.py
@@ -1,13 +1,13 @@
 import random
 import os
-
+from functools import reduce
 
 LINES_COUNT = STRING_SIZE = 10
 
 
-def random_string():
+def get_random_string():
     return reduce(lambda string, char: string + char,
-                  [chr(random.randint('a').ord('z')) for _ in range(STRING_SIZE)])
+                  [chr(random.randint(ord('a'), ord('z'))) for _ in range(STRING_SIZE)])
 
 
 def create_text_file(filename):
@@ -15,10 +15,11 @@ def create_text_file(filename):
         with open(filename, 'r', encoding='utf8') as f_descriptor:
             return f_descriptor
 
-    with open(filename, 'r', encoding='utf8') as f_descriptor:
-        numbers = [random.randint(0,100) for _ in range(LINES_COUNT)]
+    with open(filename, 'w', encoding='utf8') as f_descriptor:
+        numbers = [random.randint(0, 100) for _ in range(LINES_COUNT)]
         strings = [get_random_string() for _ in range(LINES_COUNT)]
-        descriptor.writelines([f"{number}{string}" in zip(numbers, strings)])
+        f_descriptor.writelines(
+            [f"{number} {string} \n" for number, string in zip(numbers, strings)])
         return f_descriptor
 
 
@@ -31,4 +32,4 @@ def print_text_file(f_descriptor):
 DESCRIPTOR = create_text_file('default.txt')
 
 if __name__ == '__main__':
-    print(print_text_file(DESCRIPTOR))
+    print_text_file(DESCRIPTOR)
