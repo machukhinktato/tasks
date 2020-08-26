a06c6974c86108915b96af6f77d57dbac0c65975
machukhinktato <tarabrinmv@gmail.com>
2020-08-26 19:19:08 +0300

    some tips

---
+++ orm/tips.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+class Person:
+
+    def __init__(self, firstname, secondname):
+        self.firstname = firstname
+        self.secondname = secondname
+
+    def action(self):
+        return print('person action')
+
+
+class Doctor(Person):
+    def __init__(self, firstname, secondname, age):
+        super().__init__(firstname, secondname)
+        self.age = age
+
+    def action(self):
+        super().action()
+        return print('doctor action')
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        print(datetime.now() - start)
+        return result
+    return wrapper
+
+@timer
+def matilda(name):
+    result = name
+    print(f'Hello world from {result}')
+
+
+
+
+if __name__ == '__main__':
+    matilda('mike')
+    # a = lt('Mike')
+    # print(a)
+    # a = Person('Misha', 'Tarabrin')
+    # b = Doctor('Misha', 'Tarabrin', 32)
+    # b.action()
+    # print( a.firstname, a.secondname, b.firstname, b.secondname, b.age)
